3968d0a1aa769b0e0380c83568e5d97c7ee44822
Bruno Noveli <brunimzuppwolf@gmail.com>
2020-06-30 17:08:28 -0300

    Retirada de código não utilizado

--- BingoProva/CardForm.cs
+++ BingoProva/CardForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BingoProva
@@ -20,9 +14,6 @@ namespace BingoProva
         private void generateBingoButton_Click(object sender, EventArgs e)
         {
             bingoDataGridView.DataSource = typeof(List<BingoBusiness>);
-
-
-
             bingoDataGridView.DataSource = new BingoBusiness().GenerateBingoCard();
         }
     }
